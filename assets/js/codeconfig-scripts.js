dc5caaaddfa5215351e35a139d6acfec7ae4f397
jakirmithunbd <jakirmithunbd@gmail.com>
2023-12-13 12:55:20 +0600

    pulling update

--- assets/js/codeconfig-scripts.js
+++ assets/js/codeconfig-scripts.js
@@ -21,36 +21,63 @@ function codeConfigOnLoad(){
 
 window.addEventListener('load', codeConfigOnLoad);
 
+let currentIndex = 0;
+const testimonialWrapper = document.querySelector('.cc-testimonial-wrapper');
+const testimonialContainer = document.querySelector('.testimonial-container');
+const testimonialItems = document.querySelectorAll('.testimonial-item');
 
-(function($){
-    $(document).ready(function(){
+function showTestimonial(index) {
+  const newPosition = -index * 100 + '%';
+  testimonialWrapper.style.transform = 'translateX(' + newPosition + ')';
+}
 
-        // Counter js start
-        $(document).ready(function() {
+function nextTestimonial() {
+  currentIndex = (currentIndex + 1) % testimonialItems.length;
+  showTestimonial(currentIndex);
+}
 
-            var counters = $(".cc_counter");
-            var countersQuantity = counters.length;
-            var counter = [];
+function prevTestimonial() {
+  currentIndex = (currentIndex - 1 + testimonialItems.length) % testimonialItems.length;
+  showTestimonial(currentIndex);
+}
+
+// Add event listeners for next and previous buttons
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('prevBtn').addEventListener('click', prevTestimonial);
+  document.getElementById('nextBtn').addEventListener('click', nextTestimonial);
+});
+
+
+// (function($){
+//     $(document).ready(function(){
+
+//         // Counter js start
+//         $(document).ready(function() {
+
+//             var counters = $(".cc_counter");
+//             var countersQuantity = counters.length;
+//             var counter = [];
           
-            for (i = 0; i < countersQuantity; i++) {
-              counter[i] = parseInt(counters[i].innerHTML);
-            }
+//             for (i = 0; i < countersQuantity; i++) {
+//               counter[i] = parseInt(counters[i].innerHTML);
+//             }
           
-            var cc_counter = function(start, value, id) {
-              var localStart = start;
-              setInterval(function() {
-                if (localStart < value) {
-                  localStart++;
-                  counters[id].innerHTML = localStart;
-                }
-              }, 1);
-            }
+//             var cc_counter = function(start, value, id) {
+//               var localStart = start;
+//               setInterval(function() {
+//                 if (localStart < value) {
+//                   localStart++;
+//                   counters[id].innerHTML = localStart;
+//                 }
+//               }, 1);
+//             }
           
-            for (j = 0; j < countersQuantity; j++) {
-                cc_counter(0, counter[j], j);
-            }
-          });
-        //   Counter js end 
+//             for (j = 0; j < countersQuantity; j++) {
+//                 cc_counter(0, counter[j], j);
+//             }
+//           });
+//         //   Counter js end 
 
-    });
-})(jQuery);
+//     });
+// })(jQuery);
+
