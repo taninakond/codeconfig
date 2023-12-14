645a1f790d57a8f11b180517a7bacae8385ae75d
Md. Saiful Islam <73848027+saifpust@users.noreply.github.com>
2023-12-15 02:09:43 +0600

    About us page wp done.

--- About_Us.php
+++ About_Us.php
@@ -14,8 +14,7 @@ get_header() ;?>
             <div class="heading-bg-2"></div>
             <div class="cc-shape shape-small shape-red shape-top-0 shape-left"></div>
 
-            <h1>Explore Our Solutions</h1>
-            <p>We are a team dedicated to supporting businesses in their growth journey by providing simple and affordable solutions.</p>
+            <?php $about_us_banner = get_field('about_us_banner'); if($about_us_banner){echo $about_us_banner['about_us_banner_content']; } ?>
         </div>
     </div>
 </section><!-- About Hero  -->
@@ -27,25 +26,18 @@ get_header() ;?>
 
     <div class="container">
         <div class="about-items">
+
+            <?php $about_items = get_field('about_items'); foreach($about_items as $about): ?>
             <div class="d-flex space-between align-center about-item">
                 <div class="section-title">
-                    <h2>Who <span>We Are</span></h2>
-                    <p>Elevate your WordPress journey! Our cutting-edge solutions boost performance, streamline functionality, and make it all smoother, faster, and simpler.</p>
+                    <?php echo $about['about_content']; ?>
                 </div>
                 <div class="section-image">
-                    <img src="/assets/images/our-goal.png" alt="">
+                    <img src="<?php echo $about['add_about_image']['url']; ?>" alt="<?php echo $about['add_about_image']['alt']; ?>">
                 </div>
             </div>
+            <?php endforeach; ?>
 
-            <div class="d-flex space-between align-center about-item ">
-                <div class="section-title">
-                    <h2>Our <span>Goal</span></h2>
-                    <p>We're all about crafting top-notch custom PHP applications with a focus on WordPress. Our mission is simple: deliver high-quality and scalable solutions. We bring our coding expertise to the table, incorporating the latest industry trends to create plugins that cater to eCommerce, Project Management, and Enterprise Solutions.</p>
-                </div>
-                <div class="section-image">
-                    <img src="/assets/images/our-goal.png" alt="">
-                </div>
-            </div>
         </div><!-- About Items  -->
         
     </div>
@@ -60,66 +52,20 @@ get_header() ;?>
     <div class="cc-shape shape-xsmall-2 shape-red shape-bottom-0 shape-right"></div>
     <div class="container">
         <div class="section-title text-center">
-            <h2>Let's Meet With <span>Our Team</span></h2>
-            <p>At the core of our team lies an unwavering passion for creativity and dedication. We thrive on the art of crafting exceptional products that redefine experiences.</p>
+           <?php $team_section_title = get_field('team_section_title'); if($team_section_title){echo $team_section_title['team_section_content'];} ?>
         </div>
 
         <div class="team-members flex-center flex-wrap">
-            <div class="member text-center">
-                <div class="member-photo">
-                    <img src="/assets/images/Jakirul_Islam.png" alt="">
-                </div>
-                <h4 class="name">Jakirul Islam</h4>
-                <h5 class="title">CEO & Founder</h5>
-            </div> <!-- Member  -->
 
+            <?php $team_member_list = get_field('team_member_list'); foreach($team_member_list as $member): ?>
             <div class="member text-center">
                 <div class="member-photo">
-                    <img src="/assets/images/Tanin_Ahmed.png" alt="">
+                    <img src="<?php echo $member['add_team_member_photo']['url']; ?>" alt="<?php echo $member['add_team_member_photo']['alt']; ?>">
                 </div>
-                <h4 class="name">Tanin Ahmed</h4>
-                <h5 class="title">Tanin Ahmed</h5>
+                <h4 class="name"><?php echo $member['name_of_team_member']; ?></h4>
+                <h5 class="title"><?php echo $member['title_of_team_member']; ?></h5>
             </div> <!-- Member  -->
-
-            <div class="member text-center">
-                <div class="member-photo">
-                    <img src="/assets/images/Mr_XYZ.png" alt="">
-                </div>
-                <h4 class="name">Saiful Islam</h4>
-                <h5 class="title">WordPress Developer</h5>
-            </div> <!-- Member  -->
-
-            <div class="member text-center">
-                <div class="member-photo">
-                    <img src="/assets/images/Ahnaf_Istiyad.png" alt="">
-                </div>
-                <h4 class="name">Ahanaf Istiyad</h4>
-                <h5 class="title">WordPress Developer</h5>
-            </div> <!-- Member  -->
-
-            <div class="member text-center">
-                <div class="member-photo">
-                    <img src="/assets/images/Mahmudul_Hasan.png" alt="">
-                </div>
-                <h4 class="name">Mahmudul Hasan</h4>
-                <h5 class="title">UI/UX Designer</h5>
-            </div> <!-- Member  -->
-
-            <div class="member text-center">
-                <div class="member-photo">
-                    <img src="/assets/images/Mehedy_Hasan.png" alt="">
-                </div>
-                <h4 class="name">Mehedy Hasan</h4>
-                <h5 class="title">Frontend  Developer</h5>
-            </div> <!-- Member  -->
-
-            <div class="member text-center">
-                <div class="member-photo">
-                    <img src="/assets/images/Jakirul_Islam.png" alt="">
-                </div>
-                <h4 class="name">Brian Jackson</h4>
-                <h5 class="title">CMO Of Kinsta</h5>
-            </div> <!-- Member  -->
+            <?php endforeach; ?>
         </div> <!-- team-members -->
     </div>
 </section> <!-- CodeConfig Team  -->
@@ -128,21 +74,7 @@ get_header() ;?>
     <div class="cc-shape shape-medium shape-red shape-left shape-bottom"></div>
     <div class="cc-shape shape-medium shape-blue shape-right shape-bottom"></div>
     <div class="container">
-        <div class="section-title text-center">
-            <h2>Explore <span>Our Solutions</span></h2>
-            <p>Elevate your WordPress journey! Our cutting-edge solutions boost performance, streamline functionality, and make it all smoother, faster, and simpler.</p>
-        </div>
-
-        <div class="counter-wrapper">
-            <?php $counter_list = get_field('counter_list'); foreach($counter_list as $counter): ?>
-            <div class="counter-box text-center">
-                <div class="counter-icon flex-center"><img src="<?php echo $counter['add_counter_image']['url']; ?>" alt="<?php echo $counter['add_counter_image']['alt']; ?>"></div>
-                <h2><span class="cc_counter"><?php echo $counter['counter_quantity']; ?></span><?php echo $counter['quantity_mark']; ?></h2>
-                <p><?php echo $counter['counter_title']; ?></p>
-            </div> <!-- Counter Item  -->
-            <?php endforeach ; ?>
-        </div>
-        
+        <?php get_template_part( '/counter_template_2' ); ?>
     </div>
 </section> <!-- CodeConfig Counter  -->
 
---
+++ counter_template_2.php
@@ -0,0 +1,30 @@
+<div class="section-title text-center">
+    <h2>Explore <span>Our Solutions</span></h2>
+    <p>Elevate your WordPress journey! Our cutting-edge solutions boost performance, streamline functionality, and make it all smoother, faster, and simpler.</p>
+</div>
+
+<div class="counter-wrapper">
+    <div class="counter-box text-center">
+        <div class="counter-icon flex-center"><img src="<?php echo get_theme_file_uri('/assets/images/counter-icon-1.svg') ?>" alt=""></div>
+        <h2><span class="cc_counter">1</span></h2>
+        <p>Amazing Products</p>
+    </div> <!-- Counter Item  -->
+
+    <div class="counter-box text-center">
+        <div class="counter-icon flex-center"><img src="<?php echo get_theme_file_uri('/assets/images/counter-icon-2.svg') ?>" alt=""></div>
+        <h2><span class="cc_counter">1</span></h2>
+        <p>Free Dounloads</p>
+    </div> <!-- Counter Item  -->
+
+    <div class="counter-box text-center">
+        <div class="counter-icon flex-center"><img src="<?php echo get_theme_file_uri('/assets/images/counter-icon-3.svg') ?>" alt=""></div>
+        <h2><span class="cc_counter">50</span>+</h2>
+        <p>Happy Customers</p>
+    </div> <!-- Counter Item  -->
+
+    <div class="counter-box text-center">
+        <div class="counter-icon flex-center"><img src="<?php echo get_theme_file_uri('/assets/images/counter-icon-4.svg') ?>" alt=""></div>
+        <h2><span class="cc_counter">20</span>+</h2>
+        <p>Countries Worldwide</p>
+    </div> <!-- Counter Item  -->
+</div>
